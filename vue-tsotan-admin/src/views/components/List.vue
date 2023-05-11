11d72ede1b68b60f435dd629ff424e540aad0381
khajidsuren <khajidsuren@ailab.mn>
2023-05-11 16:45:09 +0800

    dev

--- vue-tsotan-admin/src/views/components/List.vue
+++ vue-tsotan-admin/src/views/components/List.vue
@@ -1,91 +1,75 @@
 <template>
-  <div class="card col-10 px-4">
-    <div class="card-body px-0 pt-0 pb-2">
-      <div class="table-responsive p-0">
-        <table class="table align-items-center mb-0">
-          <select v-model="selectedCategory" class="my-2 text-sm font-weight-bold mb-0">
-            <option value="">All Categories</option>
-            <option v-for="category in categories" :key="category.name">{{category.name}}</option>
-          </select>
-          <thead>
-            <tr>
-              <th></th>
-              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
-              <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              ></th>
-              <th class="text-secondary opacity-7"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <li v-for="product in products" :key="product.id" class="d-flex" style="align-items: center; gap: 20px;">
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-                      <img :src=getImg(product.imgUrl)  class="w-20"/>
-                    </div>
-                  </div>
+    <div class="card col-10 px-4">
+        <div class="card-body px-0 pt-0 pb-2">
+            <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                    <select v-model="selectedCategory" class="my-2 text-sm font-weight-bold mb-0">
+                        <option value="">All Categories</option>
+                        <option v-for="category in categories" :key="category.name">{{ category.name }}</option>
+                    </select>
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
+                        <th
+                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                        ></th>
+                        <th class="text-secondary opacity-7"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <li v-for="product in products" :key="product.id" class="d-flex"
+                            style="align-items: center; gap: 20px;">
+                            <td>
+                                <div class="d-flex px-2 py-1">
+                                    <div>
+                                        <img :src=getImg(product.imgUrl) class="w-20"/>
+                                    </div>
+                                </div>
 
-                </td>
-                <td class="text-sm font-weight-bold mb-0">
-                  {{product.name}}
-                </td>
-                <td class="align-middle text-center text-sm">
-                  {{product.price}} ₮
-                </td>
-                <td class="align-middle">
-                  <a
-                    href="../edit"
-                    class="text-secondary font-weight-bold text-xs"
-                    data-toggle="tooltip"
-                    data-original-title="Edit user"
-                  >Edit</a>
-                </td>
-              </li>
-            </tr>
-          </tbody>
-          <div v-if="loading">Loading...</div>
-          <ul v-else>
-      
-        </ul>
-        </table>
-      </div>
+                            </td>
+                            <td class="text-sm font-weight-bold mb-0">
+                                {{ product.name }}
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                {{ product.price }} ₮
+                            </td>
+                            <td class="align-middle">
+                                <a
+                                        href="../edit"
+                                        class="text-secondary font-weight-bold text-xs"
+                                        data-toggle="tooltip"
+                                        data-original-title="Edit user"
+                                >Edit</a>
+                            </td>
+                        </li>
+                    </tr>
+                    </tbody>
+                    <div v-if="loading">Loading...</div>
+                    <ul v-else>
+
+                    </ul>
+                </table>
+            </div>
+        </div>
     </div>
-  </div>
 
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import categoryList from "../../../../category.json";
-  export default {
+</template>
+
+<script>
+import axios from 'axios';
+import categoryList from "../../../../category.json";
+
+export default {
     data() {
-      return {
-        products: [],
-        selectedCategory: '',
-        loading: true
-      }
+        return {
+            products: [],
+            selectedCategory: '',
+            loading: true
+        }
     },
-    computed: {
-      // categories() {
-      //   const categories = new Set();
-      //   this.products.forEach(product => {
-      //     categories.add(product.category);
-      //   });
-      //   return Array.from(categories);
-      // },
-      // filteredProducts() {
-      //   if (this.selectedCategory === '') {
-      //     return this.products;
-      //   } else {
-      //     return this.products.filter(product => {
-      //       return product.category === this.selectedCategory;
-      //     });
-      //   }
-      // }
-    },
-      mounted() {
+    mounted() {
         this.categories = categoryList.categories;
         this.fetchData();
     },
@@ -105,6 +89,6 @@
             return "data:image/png;base64," + imgUrl;
         }
     }
-  }
-  </script>
+}
+</script>
   
