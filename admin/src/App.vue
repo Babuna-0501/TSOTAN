4bb760d8dc5494f185735e90015b3e4a19b8ce78
Batdorj Delgersaikhan <batdorj.ds@gmail.com>
2023-05-27 22:06:34 +0800

    2dh admin oruullaa

---
+++ admin/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
