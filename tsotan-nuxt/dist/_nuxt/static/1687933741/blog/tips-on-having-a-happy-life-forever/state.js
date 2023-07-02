dc15f8e88ba680966231dc605edbcb27d9164baa
root <root@vps.server.local>
2023-07-02 12:30:30 +0000

    d

--- tsotan-nuxt/dist/_nuxt/static/1687933741/blog/tips-on-having-a-happy-life-forever/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687933741",layout:"default",error:a,state:{products:[],categories:[],cart:[],wishlist:[],compare:[]},serverRendered:true,routePath:"\u002Fblog\u002Ftips-on-having-a-happy-life-forever",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
